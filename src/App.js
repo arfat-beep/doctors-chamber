f27f985ab5cab91ed75297b9affc52fe71cba23b
arfat-beep <arfatrahman08@gmail.com>
2022-04-17 02:12:29 +0600

    All Routes created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Login/Login";
+import Blogs from "./Blogs/Blogs";
+import ErrorPage from "./ErrorPage/ErrorPage";
+import About from "./About/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Login/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div>thia ia login</div>;
+};
+
+export default Login;
