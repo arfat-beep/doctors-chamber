d63b23b9c6606a4a7545d2735011dc51b8cb0793
arfat-beep <arfatrahman08@gmail.com>
2022-04-17 20:16:20 +0600

    About Page complete

--- src/About/About.js
+++ src/About/About.js
@@ -1,7 +1,27 @@
 import React from "react";
-
+import "./About.css";
 const About = () => {
-  return <div>This is about page</div>;
+  return (
+    <div className="about-container">
+      <div className="about">
+        <img
+          src="https://i.ibb.co/xMT5sKG/20201225210341-IMG-1984.jpg"
+          alt=""
+        />
+        <div className="about-details">
+          <p>
+            <strong>Name : </strong> Arfatur Rahman
+          </p>
+          <p>
+            <strong>My goal :</strong>Now i'm in 4th year of B.Sc in CSE. My
+            next goal is to perfectly complete my B.Sc in CSE. I've also a
+            mission to complete this course with 720 marks and join "Programming
+            Hero Team"
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
